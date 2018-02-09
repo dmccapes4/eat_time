f75a17811d4d8fd2698633da0284755117b949f4
Dylan <dmccapes@mac.com>
2018-02-09 15:15:09 -0800

    First production push

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,11 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root';
 
-export default (preloadedState = {}) => createStore(
-  rootReducer,
-  preloadedState,
-  composeWithDevTools(applyMiddleware(thunk, logger))
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
+const configureStore = (preloadedState = {}) => (
+  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares))
 );
+
+export default configureStore;
